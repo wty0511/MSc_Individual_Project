f91c696ec040ca75b659fdd3404d33df99e4244f
wty0511 <15652198208@163.com>
2023-07-22 12:54:12 +0000

    gggg

--- src/how_to_train/train_maml_system.py
+++ src/how_to_train/train_maml_system.py
@@ -38,7 +38,7 @@ def set_seed(seed):
 SEED = 42
 set_seed(SEED)
 
-cfg = compose(config_name="configmamlpp.yaml")
+cfg = compose(config_name="configmamlpp_tnn.yaml")
 
 # Combines the arguments, model, data and experiment builders to run an experiment
 # model = MAMLFewShotClassifier(cfg)
@@ -48,7 +48,7 @@ data = MetaLearningSystemDataLoader(cfg)
 maml_system = ExperimentBuilder(cfg = cfg, model=model, data=data)
 # maml_system.run_experiment()
 
-ckpt = r"/root/task5_2023/Checkpoints/MAMLPP_2way_5step_convnetlarge_2/Model/best_model.pth"
+ckpt = r"/root/task5_2023/Checkpoints/MAMLPP_TNN10way_5step_convnetlarge_0.2_5/Model/best_model.pth"
 
 # maml_system.run_experiment()
 
